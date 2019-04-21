46499b7d87b9e08059fda0db85e660acd2fe1ffa
edenizk <dkokyanus@gmail.com>
2019-04-21 12:08:23 +0200

    social media js created

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class SocialMedia extends Component {
+    render() {
+      return (
+        <div id='social_media'>
+                <a id="git" href="https://github.com/edenizk" target="_blank">
+                    <img alt=''/>
+                </a>
+                <a id="www" href="http://bit.ly/edenizkos" target="_blank">
+                    <img alt=''/>
+                </a>
+                <a id="linkedin" href="https://www.linkedin.com/in/edkos/" target="_blank">
+                    <img alt=''/>
+                </a>
+        </div>
+      );
+    }
+  }
+
+  export default SocialMedia
